2817cd695f2e6b3b5497762df3afa917243fc1ee
Taras Prognimak <tprognimak@gmail.com>
2016-05-08 23:46:05 +0300

    Add validation

--- Products.UI/Products.UI/Scripts/popup.js
+++ Products.UI/Products.UI/Scripts/popup.js
@@ -1,7 +1,6 @@
 ﻿$(function () {
     $.ajaxSetup({ cache: false });
     $(".popupItem").click(function (e) {
-        debugger;
         e.preventDefault();
         $.get(this.href, function (data) {
             $('#dialogContent').html(data);
